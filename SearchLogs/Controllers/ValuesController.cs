f139b5c74eb632220715061255eb90c76df2bb9b
Sadykov Azat <asadykov@i-novus.ru>
2019-03-21 13:21:51 +0300

    First Version

---
+++ SearchLogs/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace SearchLogs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{pattern}")]
+        public ActionResult<string> GetLogs(string pattern)
+        {
+            var configuration = new ConfigurationBuilder()
+                                    .AddJsonFile(AppDomain.CurrentDomain.BaseDirectory + "appsettings.json")
+                                    .Build();
+            string Path = configuration["FilePath"];
+            String returnString;
+            Regex regex = new Regex(pattern);
+            using (StreamReader sr = new StreamReader(Path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (regex.Matches(line).Count > 0)
+                    {
+                        returnString = line + "\n";
+                        for (int i = 1; i < 20; i++)
+                        {
+                            returnString += sr.ReadLine() + "\n";
+                        }
+                        return returnString;
+                    }
+
+                }
+            }
+            return "Искомое значение не найдено " + pattern + " " + Path;
+        }
+    }
+}
---
+++ SearchLogs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+
+namespace SearchLogs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // получаем путь к файлу 
+            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            // путь к каталогу проекта
+            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            // создаем хост
+            var host = WebHost.CreateDefaultBuilder(args)
+                    .UseContentRoot(pathToContentRoot)
+                    .UseStartup<Startup>()
+                    .Build();
+            // запускаем в виде службы
+            host.RunAsService();
+            Console.ReadLine();
+        }
+    }
+}
